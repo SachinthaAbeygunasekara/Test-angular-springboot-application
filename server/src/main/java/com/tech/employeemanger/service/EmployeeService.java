5e3a6ca61a11746a692eac3217938984ba4dc705
SachinthaAbeygunasekara <112684802+SachinthaAbeygunasekara@users.noreply.github.com>
2024-11-05 23:25:34 +0530

    Build the project setup, employee model, repository, and service.

---
+++ server/src/main/java/com/tech/employeemanger/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package com.tech.employeemanger.service;
+
+import com.tech.employeemanger.entity.Employee;
+import com.tech.employeemanger.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return  employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Integer id){
+        return employeeRepository.findEmployeeById(id).
+                orElseThrow(() -> new UserNotFoundException("User by id "+ id + " was not found"));
+    }
+
+    public void deleteEmployee(Integer id){
+         employeeRepository.deleteEmployeeById(id);
+    }
+}
---
+++ server/src/test/java/com/tech/employeemanger/EmployeemangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.employeemanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
